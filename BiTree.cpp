efc7334a875146c450d63cd96f49cd85a75138f1
huangyingw@linux_programming <huangyingw@gmail.com>
2010-08-15 10:44:28 -0400

    n

--- BiTree.cpp
+++ BiTree.cpp
@@ -1,56 +1,11 @@
-#include<stdio.h>
-#include <iostream>
-#include<stdlib.h>
-#include<malloc.h>
-#include<string.h>
-#define MaxSize 20
-typedef char DataType;
-#include"Bitree.h"
-using std::cout;
-using std::endl;
+#include"stdio.h"
+#include"stdlib.h"
+#include<iostream>
+
+using namespace std;
+//using std::endl;
 
 int main()
 {
-	int i;
-	BiTreeNode *q[MaxSize+1];
-	BiTreeNode *root;
-	int left,right,temp;
-	int Num[MaxSize]={0};
-	char strA[MaxSize]="befcgdh";//pre_order
-	char strB[MaxSize]="febgchd";//in_order
-	char point;int n;
-	n=strlen(strA);
-	Initiate(&root);
-	for(i=0;i<n;i++)
-	{
-		point=strA[i];
-		temp=Search(strB,point);
-		left=SearchLeft(Num,temp);
-		right=SearchRight(Num,temp);
-		if(left==-1&&right==-1)
-		{
-			q[temp]=InsertLeftNode(root,point);
-			Num[temp]=1;
-		}
-		else if(left!=-1&&q[left]->rightChild==NULL)
-		{
-			q[temp]=InsertRightNode(q[left],point);
-			Num[temp]=1;
-		}
-		else if(right!=-1&&q[right]->leftChild==NULL)
-		{
-			q[temp]=InsertLeftNode(q[right],point);
-			Num[temp]=1;
-		}
-	}
-	PrintBiTree(root,0);
-	printf("****************************************\n\n");
-	printf("PreOrder: \t");
-	PreOrder(root->leftChild,Visit);
-	printf("\nInOrder:\t");
-	InOrder(root->leftChild,Visit);
-	printf("\nPostOrder: \t");
-	PostOrder(root->leftChild,Visit);
-	printf("\n*****************************************************************************\n\t\t\t\bCopyright @ 2009 Gary All Right Reserved\n");
 	return 0;
 }
