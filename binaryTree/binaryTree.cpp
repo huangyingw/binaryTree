d1366de833babb686c389b4d9411383c5e183905
huangyingw@vsts_2008_chs <huangyingw@gmail.com>
2010-08-22 10:31:25 -0400

    n

--- binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree.cpp
@@ -1,4 +1,4 @@
-// binaryTree.cpp : Defines the entry pre_Order_Index for the console application.
+// binaryTree.cpp : Defines the entry pre_Order_Char for the console application.
 //
 
 #include "stdafx.h"
@@ -19,36 +19,41 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	BiTreeNode *q[MaxSize+1];//use to 
 	BiTreeNode *root;
-	int left,right,in_Order_Index;
+	int left_search_result,right_search_result,in_Order_Index;
 	int is_Visited_Array[MaxSize]={0};//use to indicate whether a char in in_order is treated.
 	char pre_Order_Str[MaxSize]="befcgdh";//pre_order
 	char in_Order_Str[MaxSize]="febgchd";//in_order
-	char pre_Order_Index;int n;
-	n=strlen(pre_Order_Str);
+	char pre_Order_Char;
+	int pre_Order_Str_Length=strlen(pre_Order_Str);
 	Initiate(&root);
-	for(int nav=0;nav<n;nav++)
+	for(int nav=0;nav<pre_Order_Str_Length;nav++)
 	{
-		pre_Order_Index=pre_Order_Str[nav];
-		in_Order_Index=Search(in_Order_Str,pre_Order_Index);
+		pre_Order_Char=pre_Order_Str[nav];
+		in_Order_Index=Search(in_Order_Str,pre_Order_Char);
 		
-		left=SearchLeft(is_Visited_Array,in_Order_Index);
-		right=SearchRight(is_Visited_Array,in_Order_Index);
+		left_search_result=SearchLeft(is_Visited_Array,in_Order_Index);
+		right_search_result=SearchRight(is_Visited_Array,in_Order_Index);
 		
-		if(left==-1&&right==-1)//both
+		if(left_search_result==-1&&right_search_result==-1)//both
 		{
-			q[in_Order_Index]=InsertLeftNode(root,pre_Order_Index);
-			
+			q[in_Order_Index]=InsertLeftNode(root,pre_Order_Char);
+			PrintBiTree(root,0);
 		}
-		else if(left!=-1&&q[left]->rightChild==NULL)
+		else if(left_search_result!=-1&&q[left_search_result]->rightChild==NULL)
 		{
-			q[in_Order_Index]=InsertRightNode(q[left],pre_Order_Index);
+			q[in_Order_Index]=InsertRightNode(q[left_search_result],pre_Order_Char);
+			PrintBiTree(q[left_search_result],0);
 		}
-		else if(right!=-1&&q[right]->leftChild==NULL)
+		else if(right_search_result!=-1&&q[right_search_result]->leftChild==NULL)
 		{
-			q[in_Order_Index]=InsertLeftNode(q[right],pre_Order_Index);
+			q[in_Order_Index]=InsertLeftNode(q[right_search_result],pre_Order_Char);
+			PrintBiTree(q[right_search_result],0);
 		}
 		is_Visited_Array[in_Order_Index]=1;
+		
+		cout<<endl;
 	}
+	cout<<endl;
 	PrintBiTree(root,0);
 	printf("PreOrder: \t");
 	PreOrder(root->leftChild,Visit);
@@ -56,6 +61,5 @@ int _tmain(int argc, _TCHAR* argv[])
 	InOrder(root->leftChild,Visit);
 	printf("\nPostOrder: \t");
 	PostOrder(root->leftChild,Visit);
-	printf("\n");
 	return 0;
 }
