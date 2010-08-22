96bbf739e21f94c8e6add9fc59aa38e82708256c
huangyingw@vsts_2008_chs <huangyingw@gmail.com>
2010-08-22 09:29:29 -0400

    n

--- binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree.cpp
@@ -15,22 +15,16 @@ typedef char DataType;
 #include"Bitree.h"
 using namespace std;
 
-ofstream fout;
 
 void PrintNum(int Num[])
 {
 	for(int index=0;index<MaxSize;index++)
 	{
-		fout<<Num[index];
 	}
-	fout<<endl;
 }
-
-
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	fout.open("output.txt");
 
 	int i;
 	BiTreeNode *q[MaxSize+1];//use to 
@@ -44,39 +38,28 @@ int _tmain(int argc, _TCHAR* argv[])
 	Initiate(&root);
 	for(i=0;i<n;i++)
 	{
-		fout<<endl;
-		fout<<"i="<<i<<endl;
 		point=strA[i];
 		temp=Search(strB,point);
-		fout << "Search for->"<<point<<"->position in in_order->"<<temp<<endl;
 		
 		PrintNum(Num);
 		
 		left=SearchLeft(Num,temp);
-		fout << "SearchLeft result->"<<left<<endl;
 		
 		right=SearchRight(Num,temp);
-		fout << "SearchRight result->"<<right<<endl;
 		
 		if(left==-1&&right==-1)//both
 		{
 			q[temp]=InsertLeftNode(root,point);
-			fout << "InsertLeftNode->"<<point<<endl;
-			fout << "q["<<temp<<"]->"<<q[temp]->data<<endl;
 			Num[temp]=1;
 		}
 		else if(left!=-1&&q[left]->rightChild==NULL)
 		{
 			q[temp]=InsertRightNode(q[left],point);
-			fout << "InsertRightNode->"<<point<<endl;
-			fout << "q["<<temp<<"]->"<<q[temp]->data<<endl;
 			Num[temp]=1;
 		}
 		else if(right!=-1&&q[right]->leftChild==NULL)
 		{
 			q[temp]=InsertLeftNode(q[right],point);
-			fout << "InsertLeftNode->"<<point<<endl;
-			fout << "q["<<temp<<"]->"<<q[temp]->data<<endl;
 			Num[temp]=1;
 		}
 	}
@@ -89,6 +72,5 @@ int _tmain(int argc, _TCHAR* argv[])
 	printf("\nPostOrder: \t");
 	PostOrder(root->leftChild,Visit);
 	printf("\n");
-	fout.close();
 	return 0;
 }
